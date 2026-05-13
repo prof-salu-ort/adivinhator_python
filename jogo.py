97df38a7cf97cefdbee2890e00bef63cb2ec6356
prof-salu <salustiano.oliveira@ort.org.br>
2026-05-13 15:34:00 -0300

    Jogo [FINALIZADO]

--- jogo.py
+++ jogo.py
@@ -19,6 +19,10 @@ import random #biblioteca que trabalho com aleatoriedade
 #Sorteia um numero inteiro entre 1 e 100
 NUMERO_SECRETO = random.randint(1, 100)
 
+#Tentativas
+total_tentativas = 0
+numeros_chutados = ''
+
 print('Bem-vindo ao ADIVINHATOR 2026')
 print('Sorteando um número entre 1 e 100...')
 
@@ -29,13 +33,22 @@ print('*** Partida Iniciada ***')
 
 #Captura o tempo no INICIO do programa
 inicio = time.time()
-print('COLA -->', NUMERO_SECRETO)
+#print('COLA -->', NUMERO_SECRETO)
 while True:
     numero = int(input('Informe um numero [1-100]: '))
+
+    while numero < 1 or numero > 100:
+        print('Numero invalido, tente novamente...')
+        numero = int(input('Informe um numero [1-100]: '))
+
     if numero > NUMERO_SECRETO:
         print('O Numero informado é MAIOR que o NUMERO_SECRETO')
+        total_tentativas += 1
+        numeros_chutados += ' ' + str(numero)
     elif numero < NUMERO_SECRETO:
         print('O Numero informado é MENOR que o NUMERO_SECRETO')
+        total_tentativas += 1
+        numeros_chutados += ' ' + str(numero)
     else:
         print('*** Partida Finalizada ***')
         print()
@@ -45,4 +58,21 @@ while True:
 #Captura o tempo no FINAL do programa
 fim = time.time()
 
+trofeu = '''
+             ___________
+            '._==_==_=_.'
+            .-\:      /-.
+           | (|:.     |) |
+            '-|:.     |-'
+              \::.    /
+               '::. .'
+                 ) (
+               _.' '._
+              `-------`
+               VENCEDOR
+    '''
+print(trofeu)
+
 print(f'Tempo decorrido: {int(fim-inicio)} segundos')
+print('Total de tentativas:', total_tentativas)
+print('Chutes:', numeros_chutados)
